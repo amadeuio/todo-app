9273c9d79b1f4ca9e61297c4b47eaf5fd640542c
Amadeu Serras <118369464+amadeuio@users.noreply.github.com>
2024-11-26 13:08:32 +0100

    Remove template literals from script.js

--- vanilla/script.js
+++ vanilla/script.js
@@ -1,4 +1,5 @@
 import tasksData from "./data/tasks";
+import { v4 as uuidv4 } from "uuid";
 
 let tasks = tasksData;
 
@@ -9,19 +10,27 @@ const renderTasks = () => {
   tasks.forEach((task) => {
     const taskElement = document.createElement("div");
     taskElement.className = "task-item";
-    taskElement.innerHTML = `
-       <input type="checkbox" class="task-checkbox" ${
-         task.isChecked ? "checked" : ""
-       }>
-      <span class="${task.isChecked ? "line-through" : ""}">${task.title}</span>
-      <button class="delete-btn">Delete</button>
-    `;
 
-    const checkbox = taskElement.querySelector("input");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.className = "task-checkbox";
+    checkbox.checked = task.isChecked;
     checkbox.addEventListener("change", () => handleCheckboxClick(task.id));
 
-    const deleteBtn = taskElement.querySelector(".delete-btn");
+    const span = document.createElement("span");
+    span.textContent = task.title;
+    if (task.isChecked) {
+      span.className = "line-through";
+    }
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "delete-btn";
+    deleteBtn.textContent = "Delete";
     deleteBtn.addEventListener("click", () => handleDeleteClick(task.id));
+
+    taskElement.appendChild(checkbox);
+    taskElement.appendChild(span);
+    taskElement.appendChild(deleteBtn);
 
     taskList.appendChild(taskElement);
   });
@@ -44,11 +53,7 @@ const handleAddClick = (e) => {
   const taskInput = document.querySelector("#taskInput");
   const inputValue = taskInput.value.trim();
 
-  tasks.push({
-    id: tasks.length + 1,
-    title: inputValue,
-    isChecked: false,
-  });
+  tasks = [...tasks, { id: uuidv4(), title: inputValue, isChecked: false }];
 
   taskInput.value = "";
   renderTasks();
@@ -58,6 +63,3 @@ const form = document.querySelector("#addTaskBtn");
 form.addEventListener("click", handleAddClick);
 
 renderTasks();
-
-
-
